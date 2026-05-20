e4f4e0c64daf59f0db7402c7cc8817defdc003ee
parksw <parksw9659@gmail.com>
2026-05-20 16:31:06 +0900

    260520 / 박상원 / Feat / 시스템관리자 회원 등록 프로필이미지까지 완료

--- FinalProject/src/main/java/com/example/demo/setting/CustomAuthenticationDetails.java
+++ FinalProject/src/main/java/com/example/demo/setting/CustomAuthenticationDetails.java
@@ -3,7 +3,7 @@ package com.example.demo.setting;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class CustomAuthenticationDetails {
-
+	// 로그인시 기업번호 파라미터도 추가로 넣기 위한 class
     private final String bizNo;
 
     public CustomAuthenticationDetails(HttpServletRequest request) {
