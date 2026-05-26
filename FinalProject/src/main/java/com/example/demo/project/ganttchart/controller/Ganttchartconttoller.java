57ef87a018d3e371a790a9f9c9cabc1cb261638a
장수연 <sdlqm456@email.com>
2026-05-26 17:29:31 +0900

    20260526/장수연/Fix/캘린더 공휴일 표시오류해결

--- FinalProject/src/main/java/com/example/demo/project/ganttchart/controller/Ganttchartconttoller.java
+++ FinalProject/src/main/java/com/example/demo/project/ganttchart/controller/Ganttchartconttoller.java
@@ -2,9 +2,14 @@ package com.example.demo.project.ganttchart.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.login.service.UserVO;
 import com.example.demo.project.issue.service.IssueInputVO;
 import com.example.demo.project.issue.service.IssueOutputVO;
 import com.example.demo.project.issue.service.IssueService;
@@ -14,6 +19,12 @@ public class Ganttchartconttoller {
 
 	@Autowired
 	IssueService issueService;
+	
+	// 로그인한 유저 정보 가져오기
+	private UserVO getLoginUser() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return (UserVO) auth.getPrincipal();
+	}
 
 	// 간트차트 페이지 접속
 	@GetMapping("/ganttchart/list")
@@ -24,8 +35,21 @@ public class Ganttchartconttoller {
 	// 이슈목록 가져오기
 	 @GetMapping("/ganttchart/listget") 
 	 @ResponseBody
-	 public List<IssueOutputVO> getIssue() {
+	 public List<IssueOutputVO> getIssue(
+			 @RequestParam(name = "filter", defaultValue = "all") String filter) { 
+
 		 IssueInputVO param = new IssueInputVO();
+		 Long loginMemId = getLoginUser().getId();
+		 
+		 // 조회 시 개인/ 그룹 필터 추가 
+		 switch (filter) {
+		  case "my": // 개인 필터
+			  param.setMemId(loginMemId); // 로그인한 memId를 가져오기 
+			  break;
+		  case "all": // 그룹 전체 조회 
+			  default:
+				  break;
+		 }		 
 		 return issueService.selectIssueList(param);
 	 }
 
