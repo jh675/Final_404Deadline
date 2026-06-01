5a23709838274c66684a3e25404d0de095a52fb0
kabus-12 <dksgudwn15@gmail.com>
2026-06-01 14:11:17 +0900

    260601/안형주/Feat/프로젝트 복구 기능 추가,알람 1시간 전 알림 제외하고 확인완료

--- FinalProject/src/main/java/com/example/demo/project/calender/service/impl/CalenderServiceImpl.java
+++ FinalProject/src/main/java/com/example/demo/project/calender/service/impl/CalenderServiceImpl.java
@@ -2,8 +2,10 @@ package com.example.demo.project.calender.service.impl;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.alarm.event.NotificationEvent;
 import com.example.demo.project.calender.mapper.CalenderMapper;
 import com.example.demo.project.calender.service.CalenderService;
 import com.example.demo.project.calender.service.CalenderVO;
@@ -17,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 
 public class CalenderServiceImpl implements CalenderService{
   private final CalenderMapper calenderMapper;
+  private final ApplicationEventPublisher eventPublisher;
   
   @Override
   public List<CalenderVO> selectAll(CalenderVO vo) {
@@ -35,6 +38,28 @@ public class CalenderServiceImpl implements CalenderService{
   
   @Override
   public int insert(CalenderVO vo) {
+	  
+	  
+	    if ("03CALTYPE".equals(vo.getTypeCd())) {
+	        System.out.println("=== 회사일정 감지!");
+
+	        String bizNo = calenderMapper.findBizNoByMemId(vo.getMemId().longValue());
+	       
+	       
+	        if (bizNo != null) {
+	            List<String> usernames = calenderMapper.findUsernamesByBizNo(bizNo);
+	            System.out.println("=== 대상 usernames: " + usernames);
+
+	            usernames.forEach(username -> {
+	                System.out.println("=== 알림 전송 → " + username);
+	                eventPublisher.publishEvent(new NotificationEvent(
+	                    this,
+	                    "회사 일정이 등록되었습니다: " + vo.getCalText(),
+	                    username
+	                ));
+	            });
+	        }
+	    }
 	  return calenderMapper.insert(vo);
   }
   
